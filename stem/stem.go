630845063a30fa1a84b6c72e6bd4c74976b42bfb
AfoninaOlga <o.aphonina@gmail.com>
2024-03-27 17:50:58 +0300

    Change word splitting

--- stem/stem.go
+++ stem/stem.go
@@ -2,21 +2,18 @@ package main
 
 import (
 	"github.com/kljensen/snowball/english"
-	"regexp"
 	"strings"
+	"unicode"
 )
-
-func trimWord(word string) string {
-	re := regexp.MustCompile(`[^a-zA-Z]`)
-	return re.ReplaceAllString(word, "")
-}
 
 func StemInput(input string) string {
 	var result []string
 	stemmedWords := make(map[string]bool)
-	re := regexp.MustCompile(`[-'.,!?:;]`)
-	for _, s := range strings.Fields(re.ReplaceAllString(input, " ")) {
-		s := english.Stem(trimWord(s), false)
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c)
+	}
+	for _, s := range strings.FieldsFunc(input, f) {
+		s = english.Stem(s, false)
 		if len(s) <= 2 || english.IsStopWord(s) || stemmedWords[s] {
 			continue
 		}
