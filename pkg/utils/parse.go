13d33d2371d6434746e8d49115d29d826d4d19d3
AfoninaOlga <o.aphonina@gmail.com>
2024-04-05 02:09:40 +0300

    Add flag for config file

--- pkg/utils/parse.go
+++ pkg/utils/parse.go
@@ -5,9 +5,10 @@ import (
 	"math"
 )
 
-func ParseInput() (cnt uint, output bool) {
+func ParseInput() (cnt uint, output bool, configPath string) {
 	flag.UintVar(&cnt, "n", math.MaxUint, "flag takes number of comics to read")
 	flag.BoolVar(&output, "o", false, "flag determines whether to print output or not")
+	flag.StringVar(&configPath, "c", "config.yaml", "flag sets config file path")
 	flag.Parse()
 	return
 }
