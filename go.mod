324ff31b5fb4cc80ce85fb75d296b1627cf6168d
AfoninaOlga <o.aphonina@gmail.com>
2024-05-21 10:33:53 +0300

    Add sqlite adapter

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.22.1
 
 require github.com/kljensen/snowball v0.9.0
 
+require gopkg.in/yaml.v3 v3.0.1
+
 require (
+	github.com/mattn/go-sqlite3 v1.14.22
 	golang.org/x/sync v0.7.0
-	gopkg.in/yaml.v3 v3.0.1
 )
--- internal/adapter/repository/sqlite/sqlite.go
+++ internal/adapter/repository/sqlite/sqlite.go
@@ -1,1 +1,145 @@
 package sqlite
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"github.com/AfoninaOlga/xkcd/internal/core/domain"
+)
+
+type ComicDB struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *ComicDB {
+	return &ComicDB{db: db}
+}
+
+func (cdb *ComicDB) GetAll(ctx context.Context) (map[int]domain.Comic, error) {
+	comicMap := make(map[int]domain.Comic)
+	comics, err := cdb.db.QueryContext(ctx, "select id, url from Comics")
+	if err != nil {
+		return nil, err
+	}
+	defer comics.Close()
+	for comics.Next() {
+		var (
+			id       int
+			url      string
+			keywords []string
+			word     string
+		)
+		if err = comics.Scan(id, url); err != nil {
+			return nil, err
+		}
+		words, err := cdb.db.QueryContext(ctx, "select word from Indexes where id=? ", id)
+		if err != nil {
+			return nil, err
+		}
+		defer words.Close()
+		for words.Next() {
+			err = words.Scan(&word)
+			if err != nil {
+				return nil, err
+			}
+			keywords = append(keywords, word)
+		}
+		comicMap[id] = domain.Comic{Url: url, Keywords: keywords}
+	}
+	return comicMap, nil
+}
+
+func (cdb *ComicDB) GetIndex(ctx context.Context) (map[string][]int, error) {
+	return nil, nil
+}
+
+func (cdb *ComicDB) AddComic(ctx context.Context, id int, c domain.Comic) error {
+	tx, err := cdb.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	insertComic, err := tx.Prepare("INSERT INTO Comics(id, url) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	defer insertComic.Close()
+
+	insertKeyword, err := tx.Prepare("INSERT INTO Keywords(word) VALUES (?)")
+	if err != nil {
+		return err
+	}
+	defer insertKeyword.Close()
+
+	insertComicKeyword, err := tx.Prepare("INSERT INTO ComicsKeywords(comic_id, keyword_id) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+	defer insertComicKeyword.Close()
+
+	getKeyworId, err := tx.Prepare("select id from Keywords where word=?")
+	if err != nil {
+		return err
+	}
+	defer insertComicKeyword.Close()
+
+	_, err = insertComic.Exec(id, c.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, word := range c.Keywords {
+		var kId int64
+		err = getKeyworId.QueryRow(word).Scan(&kId)
+		if err == nil {
+			_, err = insertComicKeyword.Exec(id, kId)
+			if err != nil {
+				return err
+			}
+		} else if errors.Is(err, sql.ErrNoRows) {
+			if err != nil {
+				res, err := insertKeyword.Exec(word)
+				if err != nil {
+					return err
+				}
+				kId, err = res.LastInsertId()
+			}
+		} else {
+			return err
+		}
+		_, err = insertComicKeyword.Exec(id, kId)
+		if err != nil {
+			return err
+		}
+		err = tx.Commit()
+		return err
+	}
+
+	return nil
+}
+
+func (cdb *ComicDB) GetMaxId(ctx context.Context) (int, error) {
+	var id int
+	err := cdb.db.QueryRowContext(ctx, "select max(id) from Comics").Scan(&id)
+	return id, err
+}
+
+func (cdb *ComicDB) Exists(ctx context.Context, id int) (bool, error) {
+	var url string
+	err := cdb.db.QueryRowContext(ctx, "select url from Comics where id=?", id).Scan(&url)
+
+	if err == nil {
+		return true, nil
+	}
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return false, nil
+	}
+	return false, err
+}
+
+func (cdb *ComicDB) Size(ctx context.Context) (int, error) {
+	var sz int
+	err := cdb.db.QueryRowContext(ctx, "select count(*) from Comics").Scan(&sz)
+	return sz, err
+}
