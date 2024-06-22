a5ddba5ac918938f2dcb08792e8963081bca1a37
AfoninaOlga <o.aphonina@gmail.com>
2024-06-22 22:36:06 +0300

    Add main

---
+++ webserver/cmd/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"github.com/AfoninaOlga/xkcd/webserver/internal/handler"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+)
+
+func main() {
+	configPath, port := ParseFlag()
+
+	cfg, err := GetConfig(configPath)
+	if err != nil {
+		log.Fatalf("Could not read config file. Error: %v\n", err)
+	}
+
+	ttl := cfg.TokenDuration
+	if ttl == 0 {
+		ttl = 10
+	}
+	h := handler.NewHandler(cfg.Api, time.Duration(ttl)*time.Minute)
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	router := http.NewServeMux()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/comics", http.StatusSeeOther)
+	})
+	router.HandleFunc("/login", h.Login)
+	router.HandleFunc("/comics", h.Search)
+
+	httpServer := &http.Server{
+		Addr:         ":" + strconv.Itoa(port),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 20 * time.Second,
+		Handler:      router,
+		BaseContext:  func(net.Listener) context.Context { return ctx },
+	}
+
+	g, gCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		return httpServer.ListenAndServe()
+	})
+	g.Go(func() error {
+		<-gCtx.Done()
+		return httpServer.Shutdown(context.Background())
+	})
+	if err := g.Wait(); err != nil {
+		log.Printf("exit reason: %s \n", err)
+	}
+}
